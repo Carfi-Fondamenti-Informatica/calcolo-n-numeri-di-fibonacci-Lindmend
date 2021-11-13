dcb03fb0634cb7adf6d833b3b737833568073372
Lindmend <91802623+Lindmend@users.noreply.github.com>
2021-11-13 21:40:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n = 0, primo = 0, secondo = 1, terzo;
+    cin >> n;
+
+    if (n < 2) {
+        cout << "errore" << endl;
+    } else {
+        cout << "1" << endl;
+            for (int i = 0; i <=(n-2); i++) {
+            terzo = primo + secondo;
+            primo = secondo;
+            secondo = terzo;
+            cout << terzo << endl;
+        }
+
+    }
+
+    return 0;
 }
